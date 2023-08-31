4d81e3aca744a6e93c07c12ddce73c2883b22e11
xucc <951502877@qq.com>
2023-08-31 15:58:00 +0800

    feat:1、add controls  2、set listener

--- app/src/main/java/com/giftedcat/adskiphelper/MainActivity.java
+++ app/src/main/java/com/giftedcat/adskiphelper/MainActivity.java
@@ -3,12 +3,39 @@ package com.giftedcat.adskiphelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Switch swSkip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+        initViews();
     }
+
+    private void findViews(){
+        swSkip = findViewById(R.id.sw_skip);
+    }
+
+    private void initViews() {
+        swSkip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                //TODO controlling helper
+                if (isChecked){
+                    //打开跳过助手
+                }else {
+                    //关闭跳过助手
+                }
+            }
+        });
+    }
+
 }
